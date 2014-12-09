c2e806ed2620d1fd0279507221523e54277b701b
Preston Lamb <preston.j.lamb@gmail.com>
2014-12-09 07:13:04 -0700

    finished the basic login and refactoring; toastr to notify of successful login.

--- public/app/account/mvNavBarLoginCtrl.js
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -1,11 +1,12 @@
-angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http){
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope, $http, mvNotifier, mvIdentity, mvAuth){
+	$scope.identity = mvIdentity;
 	$scope.signin = function(username, password) {
-		$http.post('/login', {username: username, password: password}).then(function(res){
-			if(res.data.success) {
-				console.log('You are logged in');
+		mvAuth.authenticateUser(username, password).then(function(success){
+			if(success) {
+				mvNotifier.notify('You have successfully logged in!');
 			} else {
-				console.log('Failure');
+				mvNotifier.notify('Username/Password combination was incorrect.');
 			}
-		});
+		})
 	};
 });
