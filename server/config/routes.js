8420563e9b6adf80b67dfa11d19ce74ec1086c37
Preston Lamb <plamb@axis41.com>
2014-12-10 09:42:53 -0700

    Added a course detail page, and had it use the caching service we made previously.

--- server/config/routes.js
+++ server/config/routes.js
@@ -11,7 +11,9 @@ module.exports = function(app) {
 	app.get('/api/users', auth.requiresRole('admin'), users.getUsers);
 	app.post('/api/users', users.createUser);
 	app.put('/api/users', users.updateUser);
+
 	app.get('/api/courses', courses.getCourses);
+	app.get('/api/courses/:id', courses.getCoursesById);
 
 	app.get('/partials/*', function(req, res) {
 			res.render('../../public/app/' + req.params[0]);
